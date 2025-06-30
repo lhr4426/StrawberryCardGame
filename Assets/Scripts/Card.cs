3302e82ebed35b11b11f4fab20ed810b585a30e2
Lee Hye Rim <37866532+lhr4426@users.noreply.github.com>
2025-06-30 16:10:08 +0900

    Unity Client Updated

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public GameObject front;
+    public GameObject back;
+
+    public Animator anim;
+
+    public SpriteRenderer frontImage;
+
+    public int idx = 0;
+
+    AudioSource audioSource;
+    public AudioClip clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Setting(int number)
+    {
+        idx = number;
+        frontImage.sprite = Resources.Load<Sprite>($"rtan{idx}");
+    }
+
+    public void OpenCard()
+    {
+        audioSource.PlayOneShot(clip);
+        anim.SetBool("isOpen", true);
+        front.SetActive(true);
+        back.SetActive(false);     
+        
+        // firstCard == null => firstCard에 정보 넘기기
+        // else => secondCard에 정보 넘기기
+        // matched 부르기
+
+        if(GameManager.instance.firstCard ==  null)
+        {
+            GameManager.instance.firstCard = this;
+        }
+        else
+        {
+            GameManager.instance.secondCard = this;
+            GameManager.instance.Matched();
+        }
+    }
+
+    public void DestroyCard()
+    {
+        Invoke("DestroyCardInvoke", 0.5f);
+    }
+
+    void DestroyCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+
+    public void CloseCard()
+    {
+        Invoke("CloseCardInvoke", 0.5f);
+    }
+
+    void CloseCardInvoke()
+    {
+        anim.SetBool("isOpen", false);
+        front.SetActive(false);
+        back.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    public Card firstCard;
+    public Card secondCard;
+
+    public Text timeTxt;
+    public GameObject endTxt;
+
+    public int cardCount = 0;
+    float time = 0.0f;
+
+    AudioSource audioSource;
+    public AudioClip clip;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
+        if(time >= 30.0f)
+        {
+            GameEnd();
+        }
+    }
+
+    public void GameEnd()
+    {
+        Time.timeScale = 0f;
+        endTxt.SetActive(true);
+    }
+
+    public void Matched()
+    {
+        if (firstCard.idx == secondCard.idx)
+        {
+            audioSource.PlayOneShot(clip);
+            firstCard.DestroyCard();
+            secondCard.DestroyCard();
+            cardCount -= 2;
+            if(cardCount == 0)
+            {
+                GameEnd();
+            }
+        }
+        else
+        {
+            firstCard.CloseCard();
+            secondCard.CloseCard();
+        }
+        firstCard = null;
+        secondCard = null;
+    }
+}
