3dfeaee0a678e7b7c9b060d796ad669317ed9cba
Lee Hye Rim <37866532+lhr4426@users.noreply.github.com>
2025-07-02 15:06:10 +0900

    Follow Cursor Camera Moving Updated

--- Assets/Scripts/FollowCursor.cs
+++ Assets/Scripts/FollowCursor.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,21 @@ public class FollowCursor : MonoBehaviour
 {
     public float speed = 5f; 
     public float gameOverDistance; // 게임 오브젝트와 마우스 포인터 사이 게임오버 기준 거리
+    public float shakeDistance; // 카메라가 흔들리기 시작 할 거리
 
-    public GameObject jumpScareImage;
-    public float jumpScareDuration = .5f; 
+    public Vector3 originalCameraPosition; // 카메라 원래 위치 저장용
+
+    public GameObject jumpScareImage; // 점프스케어 이미지 오브젝트
+
+    private Tween shaking; // 카메라 흔드는거 저장해놓고 중단할 때 사용
 
     // Start is called before the first frame update
     void Start()
     {
+        originalCameraPosition = Camera.main.transform.position; 
         Vector2 size = GetComponent<SpriteRenderer>().bounds.size; // 스프라이트의 크기
         gameOverDistance = size.x / 2; // 게임 오브젝트의 크기 반으로 설정
+        shakeDistance = size.x * 2f;
     }
 
     // Update is called once per frame
@@ -33,8 +40,41 @@ public class FollowCursor : MonoBehaviour
             // 놀래키기
             jumpScareImage.SetActive(true);
             Destroy(this.gameObject); // FollowCursor 스크립트 제거
-            Invoke("OnExitButton", 1f);
+            Invoke("OnExitButton", 0.5f);
         }
+        
+        UpdateShaking(distance);
+    }
+
+    
+    public void UpdateShaking(float distance)
+    {
+        // 가까워질수록 카메라가 흔들리도록 함
+
+        float intensity = Mathf.Clamp01(1f - (distance / shakeDistance));
+        float strength = intensity * 2.0f;
+
+        if(intensity <= 0) // intensity가 0보다 작다 = 거리가 멀다
+        {
+            if (shaking != null)
+            {
+                shaking.Kill(); 
+                Camera.main.transform.position = originalCameraPosition; // 카메라 원래 위치로 되돌리기
+                shaking = null;
+            }
+            return;
+        }
+
+        if (shaking != null) shaking.Kill();
+
+        shaking = Camera.main.transform.DOShakePosition(
+            duration: 0.5f,
+            strength: strength,
+            vibrato: 30,
+            randomness: 90f,
+            snapping: false,
+            fadeOut: false
+            );
     }
 
     public void OnExitButton()
