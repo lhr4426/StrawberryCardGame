4b5ca8f25441462e4efd6f2f12befcab21172c21
adorablenewbie <youngjong2965@gmail.com>
2025-07-03 17:34:13 +0900

    체이서 만들어버렷~

--- Assets/Scripts/CursorFaster.cs
+++ Assets/Scripts/CursorFaster.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CursorFaster : MonoBehaviour
 {
     // 싱글톤 패턴 사용
     public static CursorFaster instance;
-
+    public int stageNumber = 0; // 스테이지 번호
+    public GameObject horrorChaser; // 공포 추격자 오브젝트
 
     private float speedAdder = 2.0f; // 점점 더 빠르게
 
@@ -24,7 +26,13 @@ public class CursorFaster : MonoBehaviour
     }
     public void HorrorSceneAgain()
     {
-        GameObject cursor = GameObject.FindWithTag("FollowCursor");
-        cursor.GetComponent<FollowCursor>().speed += speedAdder;
+        stageNumber++;
+        for(int i= 0; i<stageNumber; i++)
+        {
+            GameObject newChaser = Instantiate(horrorChaser, new Vector3(Random.Range(-2.0f, 2.0f), Random.Range(-3.0f, 3.0f), 0), Quaternion.identity);
+            newChaser.GetComponent<FollowCursor>().speed += speedAdder * stageNumber; // 속도 증가
+        }
     }
+
+
 }
